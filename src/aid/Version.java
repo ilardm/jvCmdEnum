093f2be12cd53d21a0d9a8280e10c0a6c6695b08
Ilya Arefiev <arefiev.id@gmail.com>
2012-02-03 00:27:06 +0400

    add ant build file

---
+++ src/aid/Version.java
@@ -0,0 +1,7 @@
+package aid;
+
+public final class Version {
+	public static final float VERSION=0.1F;
+	public static final int VERSION_BUILD=0;
+	public static final String VERSION_FULL = VERSION + "." + VERSION_BUILD;
+}
