f85423b8ff2c3578cdf2269ac6885f00df34971f
Ivan <vanrazin@gmail.com>
2012-02-03 21:50:17 +0300

    Update Shell class. Added Login class and Commands enum

--- src/aid/Main.java
+++ src/aid/Main.java
@@ -1,12 +1,13 @@
 package aid;
 
 public class Main {
+	
+	private boolean isAuthorized = false;
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println("+++ startup");
+		Shell shell = new Shell();
+		shell.run();
 	}
 
 }
